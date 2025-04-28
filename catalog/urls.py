c83d80fe7f577f9a61861ba4392b939835b2f40c
Petr Maev <petr.maev94@gmail.com>
2025-04-28 20:57:06 +0300

    add application 'catalog'

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('home/', views.home_page, name='home'),
+    path('contacts/', views.contacts_page, name='contacts')
+]
