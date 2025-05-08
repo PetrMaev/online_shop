6a87a05b947eb05b2174a9ef8717c9e6ef4550c3
Petr Maev <petr.maev94@gmail.com>
2025-05-08 15:22:13 +0300

    add new controllers

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Product, Category
+
 
 def home_page(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/home.html', context=context)
 
 
 def contacts_page(request):
@@ -14,3 +18,25 @@ def contacts_page(request):
 
         return HttpResponse(f"Спасибо {name}! Сообщение отправлено.")
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        'product_name': product.product_name,
+        'description': product.description,
+        'price': product.price,
+    }
+    return render(request, 'catalog/product_detail.html', context=context)
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/product_list.html', context=context)
+
+
+def categories_list(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'catalog/categories_list.html', context=context)
