e21a115849e41192b2e154dab50c82eec1bbaf41
Petr Maev <petr.maev94@gmail.com>
2025-05-11 22:56:34 +0300

    add blog in installed_apps

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "catalog",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
