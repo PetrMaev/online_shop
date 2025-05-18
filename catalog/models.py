ee7830375c4bcb234dbdd93d36d9990bdf121190
Petr Maev <petr.maev94@gmail.com>
2025-05-18 16:56:22 +0300

    add new conditions for attributes

--- catalog/models.py
+++ catalog/models.py
@@ -2,31 +2,44 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100, verbose_name='Наименование категории')
-    description = models.TextField(verbose_name='Описание')
+    category_name = models.CharField(
+        max_length=100, verbose_name="Наименование категории"
+    )
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
 
     def __str__(self):
         return self.category_name
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
-        ordering = ['category_name',]
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = [
+            "category_name",
+        ]
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='images/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories')
-    price = models.IntegerField()
+    product_name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+    image = models.ImageField(
+        upload_to="images/", null=True, blank=True, verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="categories",
+        verbose_name="Категория",
+    )
+    price = models.IntegerField(verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.product_name}'
+        return f"{self.product_name}"
 
     class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-        ordering = ['product_name',]
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = [
+            "product_name",
+        ]
