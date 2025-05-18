6465503ae03c762a9edcb684408a94a570da3fc9
Petr Maev <petr.maev94@gmail.com>
2025-05-18 16:54:35 +0300

    add urls for CRUD-operations

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from .views import ProductsListView, ProductDetailVew, CategoriesListVew, ContactsView
+
+from .views import (CategoriesListVew, ContactsView, ProductCreateView,
+                    ProductDeleteView, ProductDetailVew, ProductsListView,
+                    ProductUpdateView)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='home'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product_detail/<int:pk>/', ProductDetailVew.as_view(), name='product_detail'),
-    path('categories_list/', CategoriesListVew.as_view(), name='categories_list'),
+    path("", ProductsListView.as_view(), name="home"),
+    path("catalog/new/", ProductCreateView.as_view(), name="product_create"),
+    path("catalog/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
+    path("catalog/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
+    path("catalog/contacts/", ContactsView.as_view(), name="contacts"),
+    path("catalog/product_detail/<int:pk>/", ProductDetailVew.as_view(),name="product_detail"),
+    path("catalog/categories_list/", CategoriesListVew.as_view(), name="categories_list"),
 ]
