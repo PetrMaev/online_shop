8b17496897769eca36e3d68ad4e1d55788ccce70
Petr Maev <petr.maev94@gmail.com>
2025-06-07 01:39:54 +0300

    add url for new view

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,19 +2,20 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 
-from .views import (CategoriesListVew, ContactsView, ProductCreateView,
+from .views import (CategoriesListView, ContactsView, ProductCreateView,
                     ProductDeleteView, ProductDetailVew, ProductsListView,
-                    ProductUpdateView, ProductsAllListView)
+                    ProductUpdateView, ProductsAllListView, CategoryDetailView)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductsListView.as_view(), name="home"),
+    path("category_detail/<int:category_id>/", CategoryDetailView.as_view(), name="category_detail"),
     path("catalog/new/", ProductCreateView.as_view(), name="product_create"),
     path("catalog/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
     path("catalog/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
     path("catalog/contacts/", ContactsView.as_view(), name="contacts"),
     path("catalog/product_detail/<int:pk>/", ProductDetailVew.as_view(),name="product_detail"),
     path("catalog/product_list_all/", ProductsAllListView.as_view(),name="product_list_all"),
-    path("catalog/categories_list/", CategoriesListVew.as_view(), name="categories_list"),
+    path("catalog/categories_list/", CategoriesListView.as_view(), name="categories_list"),
 ]
