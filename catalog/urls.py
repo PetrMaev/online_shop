f667dbca41118983e3e92805a80c5d9e0342582e
Petr Maev <petr.maev94@gmail.com>
2025-06-07 12:33:56 +0300

    edit homepage

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,13 +9,14 @@ from .views import (CategoriesListView, ContactsView, ProductCreateView,
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", ProductsListView.as_view(), name="home"),
+    path("", CategoriesListView.as_view(), name="home"),
     path("category_detail/<int:category_id>/", CategoryDetailView.as_view(), name="category_detail"),
     path("catalog/new/", ProductCreateView.as_view(), name="product_create"),
     path("catalog/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
     path("catalog/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
     path("catalog/contacts/", ContactsView.as_view(), name="contacts"),
     path("catalog/product_detail/<int:pk>/", ProductDetailVew.as_view(),name="product_detail"),
+    path("catalog/product_list/", ProductsListView.as_view(),name="product_list"),
     path("catalog/product_list_all/", ProductsAllListView.as_view(),name="product_list_all"),
     path("catalog/categories_list/", CategoriesListView.as_view(), name="categories_list"),
 ]
